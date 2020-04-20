2224794ced48ce9c5a57ad843478777c85c586b4
Fabian Neumann <fabian.neumann@outlook.de>
2020-04-20 11:15:20 +0200

    remove print statement

--- scripts/generate_alternative.py
+++ scripts/generate_alternative.py
@@ -143,9 +143,6 @@ def define_mga_constraint(n, sns):
 
     define_constraints(n, lhs, "<=", rhs, "GlobalConstraint", "mu_epsilon")
 
-    # maximum objective value (relative)
-    print(rhs / n.objective)
-
 
 def define_mga_objective(n):
 
